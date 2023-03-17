ef2f9229f37c12c24b7e6892ae4da670ff4120b0
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-17 19:58:28 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace He_Guesses;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("ADIVINHA");
+        Console.WriteLine();
+        Console.WriteLine("Sabe o que é pior que ser atingido por um raio?");
+        Thread.Sleep(5000);
+        Console.WriteLine();
+        Console.WriteLine("Ser atingido por um diâmetro, que tem o dobro de tamanho!");
+        Thread.Sleep(3000);
+    }
+}
